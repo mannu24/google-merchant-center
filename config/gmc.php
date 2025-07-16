8e58d0762ce16753e3736d15bca80797f5d2f35a
Manu <mannukumarshah@gmail.com>
2025-07-16 16:42:31 +0530

    delete

--- config/gmc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'merchant_id' => env('GMC_MERCHANT_ID', ''),
-    'service_account_json' => storage_path('gmc_service_account.json'),
-];
--- src/Repositories/Interfaces/GMCRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Manu\GMCIntegration\Repositories\Interfaces;
-
-interface GMCRepositoryInterface
-{
-    public function uploadProduct(array $productData);
-    public function deleteProduct(string $productId);
-}
